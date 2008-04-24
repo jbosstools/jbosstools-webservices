cb536fb7a87ad6b94efd0ae457eac8d9b6ae93a4
Feng Qian <fqian@redhat.com>
2008-04-24 07:31:27 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@7723 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jboss.tools.ws.creation.ui/src/org/jboss/tools/ws/creation/ui/widgets/Java2WSDLCodeGenConfigWidget.java
@@ -0,0 +1,65 @@
+package org.jboss.tools.ws.creation.ui.widgets;
+
+
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wst.command.internal.env.ui.widgets.SimpleWidgetDataContributor;
+import org.eclipse.wst.command.internal.env.ui.widgets.WidgetDataEvents;
+import org.jboos.tools.ws.creation.core.data.ServiceModel;
+
+public class Java2WSDLCodeGenConfigWidget extends SimpleWidgetDataContributor {
+	
+	private ServiceModel model;
+
+	public Java2WSDLCodeGenConfigWidget(ServiceModel model){
+		this.model = model;
+	}
+	
+	public WidgetDataEvents addControls( Composite parent, Listener statusListener){
+		
+		Composite configCom = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout(2, false);		
+		configCom.setLayout(layout);
+		configCom.setLayoutData(new GridData(GridData.FILL_BOTH));
+		Label lblCustomPakage = new Label(configCom, SWT.NONE);
+		lblCustomPakage.setText("Custom package name:");
+		final Text txtCustomPkgName = new Text(configCom, SWT.NONE);
+		txtCustomPkgName.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		txtCustomPkgName.addModifyListener(new ModifyListener(){
+
+			public void modifyText(ModifyEvent e) {
+				model.setCustomPackage(txtCustomPkgName.getText());
+			}});
+		
+		new Label(configCom, SWT.NONE).setText("Binding file:");
+		Composite fileSelection = new Composite(configCom, SWT.NONE);
+		fileSelection.setLayout(new GridLayout(2, false));
+		fileSelection.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		final Text txtFiles = new Text(fileSelection, SWT.NONE);
+		txtFiles.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		Button btnSelect = new Button(fileSelection, SWT.NONE);
+		btnSelect.setText("...");
+		btnSelect.addSelectionListener(new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent e) {
+				String fileLocation = new FileDialog(Display.getCurrent().getActiveShell(), SWT.NONE).open();
+				txtFiles.setText(fileLocation);
+				model.setBindingFileLcation(fileLocation);
+			}
+		});
+		
+		return this;
+	}
+}
---
+++ plugins/org.jboss.tools.ws.creation.ui/src/org/jboss/tools/ws/creation/ui/wsrt/JBossWSJava2WSDLConfigWidgetFactory.java
@@ -0,0 +1,75 @@
+package org.jboss.tools.ws.creation.ui.wsrt;
+
+import org.eclipse.wst.command.internal.env.core.data.DataMappingRegistry;
+import org.eclipse.wst.command.internal.env.ui.widgets.INamedWidgetContributor;
+import org.eclipse.wst.command.internal.env.ui.widgets.INamedWidgetContributorFactory;
+import org.eclipse.wst.command.internal.env.ui.widgets.SimpleWidgetContributor;
+import org.eclipse.wst.command.internal.env.ui.widgets.WidgetContributor;
+import org.eclipse.wst.command.internal.env.ui.widgets.WidgetContributorFactory;
+import org.eclipse.wst.command.internal.env.ui.widgets.WidgetDataContributor;
+import org.jboos.tools.ws.creation.core.commands.InitialCommnad;
+import org.jboos.tools.ws.creation.core.data.ServiceModel;
+import org.jboss.tools.ws.creation.ui.widgets.CodeGenConfigWidget;
+
+public class JBossWSJava2WSDLConfigWidgetFactory implements
+		INamedWidgetContributorFactory {
+
+	private SimpleWidgetContributor servicesXMLSelectWidgetContrib;
+	private ServiceModel model;
+
+	public JBossWSJava2WSDLConfigWidgetFactory() {
+	}
+
+	public INamedWidgetContributor getFirstNamedWidget() {
+		if (servicesXMLSelectWidgetContrib == null)
+			init();
+		return servicesXMLSelectWidgetContrib;
+	}
+
+	public INamedWidgetContributor getNextNamedWidget(
+			INamedWidgetContributor widgetContributor) {
+		if (servicesXMLSelectWidgetContrib == null)
+			init();
+		INamedWidgetContributor nextWidgetContrib = null;
+		return nextWidgetContrib;
+	}
+
+	public void registerDataMappings(DataMappingRegistry dataRegistry) {
+		// Map the data model from the defaulting command to this widget
+		// factory.
+		// The framework will actually to the call to getWebServiceDataModel in
+		// the ExampleDefaultingCommand class and then call the
+		// setWebServiceDataModel
+		// method in this class.
+		dataRegistry.addMapping(InitialCommnad.class, "WebServiceDataModel", //$NON-NLS-1$
+				JBossWSJava2WSDLConfigWidgetFactory.class);
+	}
+
+	public void setWebServiceDataModel(ServiceModel model) {
+		this.model = model;
+	}
+
+	private void init() {
+		// Pages of Axis2 Web Services Java Bean Scenario
+		CodeGenConfigWidget servicesXMLSelectWidget = new CodeGenConfigWidget(
+				model);
+		servicesXMLSelectWidgetContrib = createWidgetContributor(
+				"JBoss Web Service Code Generation Configuration",
+				"Please input the appropriate option for the code generation",
+				servicesXMLSelectWidget);
+	}
+
+	private SimpleWidgetContributor createWidgetContributor(String title,
+			String description, final WidgetDataContributor contributor) {
+		SimpleWidgetContributor widgetContrib = new SimpleWidgetContributor();
+		widgetContrib.setTitle(title);
+		widgetContrib.setDescription(description);
+		widgetContrib.setFactory(new WidgetContributorFactory() {
+			public WidgetContributor create() {
+				return contributor;
+			}
+		});
+		return widgetContrib;
+	}
+
+}
