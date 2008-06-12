aa5d15fa87f0d9c9b7e26c8d3b4d7b1e3a8400cf
Feng Qian <fqian@redhat.com>
2008-06-12 07:13:33 +0000

    JBIDE-2047: set default server and runtime: jbossws and jboss server for jboss web service generation
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8737 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.ws.core/src/org/jboss/tools/ws/core/JbossWSCorePlugin.java
+++ plugins/org.jboss.tools.ws.core/src/org/jboss/tools/ws/core/JbossWSCorePlugin.java
@@ -39,6 +39,9 @@ public class JbossWSCorePlugin extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		org.eclipse.jst.ws.internal.consumption.ui.plugin.WebServiceConsumptionUIPlugin.getInstance().getPluginPreferences().setDefault("PREFERENCE_SERVER", "org.eclipse.jst.server.generic.jboss42");
+		org.eclipse.jst.ws.internal.consumption.ui.plugin.WebServiceConsumptionUIPlugin.getInstance().getPluginPreferences().setDefault("PREFERENCE_RUNTIME", "org.jboss.tools.ws.creation.jbossWebServiceRT");
+
 	}
 
 	/*
