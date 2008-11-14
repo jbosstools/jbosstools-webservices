239c5f818cfd97433d2b0a87ee6c1296c5420852
Feng Qian <fqian@redhat.com>
2008-11-14 06:49:46 +0000

    JBIDE-3173:Can not load class when generate Web Service code using JBossWS as web service runtime
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@11780 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.ws.creation.core/src/org/jboss/tools/ws/creation/core/commands/WSProviderInvokeCommand.java
+++ plugins/org.jboss.tools.ws.creation.core/src/org/jboss/tools/ws/creation/core/commands/WSProviderInvokeCommand.java
@@ -11,22 +11,38 @@
 
 package org.jboss.tools.ws.creation.core.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IClasspathContainer;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.core.JavaProject;
+import org.eclipse.jst.server.core.internal.RuntimeClasspathContainer;
+import org.jboss.tools.ws.core.classpath.JBossWSRuntimeClassPathInitializer.JBossWSRuntimeClasspathContainer;
 import org.jboss.tools.ws.creation.core.data.ServiceModel;
 import org.jboss.tools.ws.creation.core.utils.JBossWSCreationUtils;
 
 /**
  * @author Grid Qian
  */
+@SuppressWarnings("restriction")
 public class WSProviderInvokeCommand extends AbstractGenerateCodeCommand {
 
-	private static String WSPROVIDER_FILE_NAME_LINUX = "wsprovide.sh"; 
+	private static String WSPROVIDER_FILE_NAME_LINUX = "wsprovide.sh";
 	private static String WSPROVIDER_FILE_NAME_WIN = "wsprovide.bat";
-	
+	private static String SEPERATOR_WIN = ";";
+	private static String SEPERATOR_LINUX = ":";
+	private String seperator;
+
 	public WSProviderInvokeCommand(ServiceModel model) {
 		super(model);
 	}
-
 
 	@Override
 	protected String getCommandLineFileName_linux() {
@@ -40,28 +56,81 @@ public class WSProviderInvokeCommand extends AbstractGenerateCodeCommand {
 
 	@Override
 	protected String getCommandlineArgs() {
-		String commandLine;
+		if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
+			seperator = SEPERATOR_WIN;
+		} else {
+			seperator = SEPERATOR_LINUX;
+		}
+		StringBuffer commandLine = new StringBuffer();
 		String project = model.getWebProjectName();
 		String projectRoot = JBossWSCreationUtils.getProjectRoot(project)
 				.toOSString();
-		commandLine = "-s " + projectRoot + Path.SEPARATOR + "src";
 
-		if (model.isGenWSDL()) {
-			commandLine += " -w ";
+		IProject iProject = ResourcesPlugin.getWorkspace().getRoot()
+				.getProject(project);
+		IJavaProject javaProject = JavaCore.create(iProject);
+
+		commandLine.append(" -s ").append(projectRoot).append(Path.SEPARATOR)
+				.append("src");
+
+		try {
+			commandLine.append(" -c ").append("\"");
+
+			commandLine.append(projectRoot).append(Path.SEPARATOR).append(
+					javaProject.getOutputLocation().removeFirstSegments(1)
+							.toOSString()).append(seperator);
+			commandLine.append(getClasspathEntries(javaProject)).append("\" ");
+			commandLine.append(" -o ").append(projectRoot).append(
+					Path.SEPARATOR).append(
+					javaProject.getOutputLocation().removeFirstSegments(1)
+							.toOSString());
+		} catch (JavaModelException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
+		if (model.isGenWSDL()) {
+			commandLine.append(" -w ");
+		}
+		commandLine.append(" -r ").append(projectRoot).append(Path.SEPARATOR)
+				.append("WebContent").append(Path.SEPARATOR).append("wsdl ");
+		commandLine.append(model.getServiceClasses().get(0));
 
-		commandLine += " -o " + projectRoot + Path.SEPARATOR
-				+ "build/classes/ ";
+		return commandLine.toString();
 
-		commandLine += " -r " + projectRoot + Path.SEPARATOR + "WebContent"
-				+ Path.SEPARATOR + "wsdl ";
+	}
 
-		commandLine += " -c " + projectRoot + Path.SEPARATOR
-				+ "build/classes/ ";
-
-		commandLine += model.getServiceClasses().get(0);
-
-		return commandLine;
+	private String getClasspathEntries(IJavaProject javaProject) {
+		IClasspathEntry[] iniEntries = null;
+		List<IClasspathEntry> pathList = new ArrayList<IClasspathEntry>();
+		IClasspathEntry[] resolvedEntries = null;
+		try {
+			iniEntries = javaProject.getRawClasspath();
+			for (IClasspathEntry entry : iniEntries) {
+				IClasspathContainer container = JavaCore.getClasspathContainer(
+						entry.getPath(), javaProject);
+				if (!(container instanceof JBossWSRuntimeClasspathContainer)) {
+					if (!(container instanceof RuntimeClasspathContainer && container
+							.getDescription().contains("JBoss"))) {
+						pathList.add(entry);
+					}
+				}
+			}
+			resolvedEntries = ((JavaProject) javaProject)
+					.resolveClasspath(pathList
+							.toArray(new IClasspathEntry[pathList.size()]));
+		} catch (JavaModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		StringBuffer path = new StringBuffer();
+		for (IClasspathEntry entry : resolvedEntries) {
+			if (entry.getEntryKind() == 3 || entry.getEntryKind() == 2) {
+				// path.append(workspaceRoot);
+				continue;
+			}
+			path.append(entry.getPath().toOSString()).append(seperator);
+		}
+		return path.toString();
 
 	}
 }
