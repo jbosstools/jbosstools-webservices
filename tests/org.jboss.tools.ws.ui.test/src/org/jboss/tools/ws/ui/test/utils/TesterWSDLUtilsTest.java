4a543ddf49d04bd19b3ff619001668822960502f
Lukas Jungmann <ljungman@redhat.com>
2010-07-22 14:07:44 +0000

    test for JBIDE-6694
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@23667 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.ws.ui.test/src/org/jboss/tools/ws/ui/test/utils/TesterWSDLUtilsTest.java
+++ tests/org.jboss.tools.ws.ui.test/src/org/jboss/tools/ws/ui/test/utils/TesterWSDLUtilsTest.java
@@ -1,12 +1,12 @@
-/******************************************************************************* 
- * Copyright (c) 2010 Red Hat, Inc. 
- * Distributed under license by Red Hat, Inc. All rights reserved. 
- * This program is made available under the terms of the 
- * Eclipse Public License v1.0 which accompanies this distribution, 
- * and is available at http://www.eclipse.org/legal/epl-v10.html 
- * 
- * Contributors: 
- * Red Hat, Inc. - initial API and implementation 
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
 package org.jboss.tools.ws.ui.test.utils;
 
@@ -28,7 +28,7 @@ public class TesterWSDLUtilsTest {
 		Definition def = readWSDL("/jbide6558/x.wsdl");
 		String[] r = TesterWSDLUtils.getNSServiceNameAndMessageNameArray(def, "HelloWorldService", "HelloWorldPort", "HelloWorldBinding", "sayHello");
 		Assert.assertArrayEquals(new String[] {"http://webservices.samples.jboss.org/", "HelloWorldService", "HelloWorldPort"}, r);
-		
+
 		def = readWSDL("/jbide6593/original.wsdl");
 		r = TesterWSDLUtils.getNSServiceNameAndMessageNameArray(def, "gsearch_rss", "gsearch_rssSoap", "gsearch_rssSoap", "GetSearchResults");
 		Assert.assertArrayEquals(new String[] {"http://www.ecubicle.net/webservices", "gsearch_rss", "gsearch_rssSoap"}, r);
@@ -46,7 +46,7 @@ public class TesterWSDLUtilsTest {
 		def = readWSDL("/jbide6593/original.wsdl");
 		r = TesterWSDLUtils.getEndpointURL(def, "gsearch_rss", "gsearch_rssSoap", "gsearch_rssSoap", "GetSearchResults");
 		Assert.assertEquals("http://www.ecubicle.net/gsearch_rss.asmx", r);
-		
+
 		r = TesterWSDLUtils.getEndpointURL(def, "EchoService", "EchoPort", "EchoPortBinding", "echo");
 		Assert.assertEquals("http://localhost:8080/webws/EchoPortType", r);
 	}
@@ -75,9 +75,9 @@ public class TesterWSDLUtilsTest {
 		Assert.assertTrue("was: '" + s1, s1.contains("<p><age>?</age>"));
 		Assert.assertTrue("was: '" + s1, s1.contains("<male>?</male>"));
 		Assert.assertTrue("was: '" + s1, s1.contains("<tax>?</tax>"));
-		Assert.assertTrue("was: '" + s1, s1.contains("<echo xmlns = \"http://test.jboss.org/ns\">"));		
-	}	
-	
+		Assert.assertTrue("was: '" + s1, s1.contains("<echo xmlns = \"http://test.jboss.org/ns\">"));
+	}
+
 	@Test
 	public void testJBIDE6558() {
 		String s1 = getSampleMessage("/jbide6558/x.wsdl", "HelloWorldService", "HelloWorldPort", "HelloWorldBinding", "sayHello");
@@ -86,7 +86,7 @@ public class TesterWSDLUtilsTest {
 		String s2 = getSampleMessage("/jbide6558/y.wsdl", "HelloWorldService", "HelloWorldPort", "HelloWorldBinding", "sayHello");
 		Assert.assertEquals(s1, s2);
 	}
-	
+
 	@Test
 	public void testJBIDE6593() {
 		String s1 = getSampleMessage("/jbide6497/original.wsdl", "gsearch_rss", "gsearch_rssSoap", "gsearch_rssSoap", "GetSearchResults");
@@ -97,11 +97,20 @@ public class TesterWSDLUtilsTest {
 		Assert.assertEquals(s1, s2);
 	}
 
+	@Test
+	public void testJBIDE6694() {
+		String s1 = getSampleMessage("/jbide6694.wsdl", "Converter", "ConverterPort", "ConverterBinding", "convert");
+		Assert.assertTrue(s1.contains("<ChangeUnit xmlns = \"http://test.jboss.org/ns\">"));
+		Assert.assertTrue(s1.contains("<value>?</value>"));
+		Assert.assertTrue(s1.contains("<fromUnit>?</fromUnit>"));
+		Assert.assertTrue(s1.contains("<toUnit>?</toUnit>"));
+	}
+
 	private String getSampleMessage(String res, String service, String port, String binding, String operation) {
 		Definition def = readWSDL(res);
 		return TesterWSDLUtils.getSampleSOAPInputMessage(def, service, port, binding, operation);
 	}
-	
+
 	private Definition readWSDL(String path) {
 		try {
 			URL url = TesterWSDLUtilsTest.class.getResource(path).toURI().toURL();
