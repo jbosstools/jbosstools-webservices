c3b18feadf3273b6fa24d25f09fc0513bd0538ea
Xavier Coulon <xcoulon@gmail.com>
2012-02-09 20:54:32 +0000

    Open - Hook up WS Tester to the JAX-RS tooling https://issues.jboss.org/browse/JBIDE-10609
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@38578 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.ws.jaxrs.ui/src/org/jboss/tools/ws/jaxrs/ui/cnf/action/OpenInWSTesterAction.java
+++ plugins/org.jboss.tools.ws.jaxrs.ui/src/org/jboss/tools/ws/jaxrs/ui/cnf/action/OpenInWSTesterAction.java
@@ -14,6 +14,9 @@ package org.jboss.tools.ws.jaxrs.ui.cnf.action;
 import java.lang.reflect.Method;
 import java.util.List;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
@@ -26,12 +29,15 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.navigator.CommonViewer;
+import org.eclipse.wst.server.core.IModule;
+import org.eclipse.wst.server.core.IServer;
+import org.eclipse.wst.server.core.ServerUtil;
 import org.jboss.tools.ws.jaxrs.ui.cnf.UriPathTemplateElement;
 import org.jboss.tools.ws.jaxrs.ui.internal.utils.Logger;
 
 /**
  * @author bfitzpat
- *
+ * 
  */
 public class OpenInWSTesterAction extends Action implements ISelectionChangedListener {
 
@@ -43,6 +49,7 @@ public class OpenInWSTesterAction extends Action implements ISelectionChangedLis
 
 	/**
 	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.jface.action.Action#run()
 	 */
 	@Override
@@ -57,19 +64,17 @@ public class OpenInWSTesterAction extends Action implements ISelectionChangedLis
 		try {
 			if (selectedObject instanceof UriPathTemplateElement) {
 				UriPathTemplateElement element = (UriPathTemplateElement) selectedObject;
-				String uriPathTemplate = element.getEndpoint().getUriPathTemplate();
-				String uriPrefix = "http://[domain]:[port]"; //$NON-NLS-1$
-				
+				String endpointUri = computeEndpointURI(element);
 				// Now we call the WS Tester through Reflection so there's no direct plug-in dependency
 				IWorkbench wb = PlatformUI.getWorkbench();
 				IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
-				IWorkbenchPage page = win.getActivePage();				
+				IWorkbenchPage page = win.getActivePage();
 				String id = "org.jboss.tools.ws.ui.tester.views.TestWSView"; //$NON-NLS-1$
 				try {
 					IViewPart part = page.showView(id);
-					if (part != null && 
-							part.getClass().getName().equalsIgnoreCase(
-									"org.jboss.tools.ws.ui.views.JAXRSWSTestView2")) { //$NON-NLS-1$
+					if (part != null
+							&& part.getClass().getName()
+									.equalsIgnoreCase("org.jboss.tools.ws.ui.views.JAXRSWSTestView2")) { //$NON-NLS-1$
 						Class<?> c = part.getClass();
 						Class<?> parmtypes[] = new Class[2];
 						parmtypes[0] = String.class;
@@ -77,10 +82,10 @@ public class OpenInWSTesterAction extends Action implements ISelectionChangedLis
 						Method setURL = c.getDeclaredMethod("setJAXRS", parmtypes); //$NON-NLS-1$
 						if (setURL != null) {
 							Object arglist[] = new Object[2];
-				            arglist[0] =  uriPrefix + uriPathTemplate;
-				            arglist[1] = element.getEndpoint().getHttpMethod().getHttpVerb();
-				            setURL.invoke(part, arglist);
-				        }
+							arglist[0] = endpointUri;
+							arglist[1] = element.getEndpoint().getHttpMethod().getHttpVerb();
+							setURL.invoke(part, arglist);
+						}
 					}
 				} catch (PartInitException e) {
 					e.printStackTrace();
@@ -90,6 +95,29 @@ public class OpenInWSTesterAction extends Action implements ISelectionChangedLis
 		} catch (Exception e) {
 			Logger.error("Failed to open WS Tester", e);
 		}
+	}
+
+	/**
+	 * Creates the endpoint URI from the endpoint, by looking at the servers on which the module is deployed.
+	 * @param element
+	 * @return
+	 */
+	private String computeEndpointURI(UriPathTemplateElement element) {
+		String uriPathTemplate = element.getEndpoint().getUriPathTemplate();
+		IProject project = element.getEndpoint().getJavaProject().getProject();
+		IModule module = ServerUtil.getModule(project);
+		IPath path = new Path(module.getName()).append(uriPathTemplate);
+		// WebResource wr = new WebResource(module, path);
+		// check to see if this project has been deployed...
+		IServer[] servers = ServerUtil.getServersByModule(module, null);
+		if (servers == null || servers.length == 0) {
+			return "http://[domain]:[port]/" + path.toPortableString(); //$NON-NLS-1$
+		}
+		// if it's been deployed, we can grab the domain and web port
+		String domain = servers[0].getHost();
+		String webport = servers[0].getAttribute("org.jboss.ide.eclipse.as.core.server.webPort", "8080");//$NON-NLS-1$ //$NON-NLS-2$
+		return "http://" + domain + ':' + webport + "/" + path.toPortableString(); //$NON-NLS-1$
+
 	}
 
 	@Override
