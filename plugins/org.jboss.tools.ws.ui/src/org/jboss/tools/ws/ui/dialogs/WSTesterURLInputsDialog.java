63c6b10a4639ccf97fc9950f576c1edb9a11f49d
Brian Fitzpatrick <bfitzpat@redhat.com>
2012-05-16 19:24:17 +0000

    JBIDE-11728 - Fixing issue with missing / in http:/ when testing JAX-RS methods with parms (also handling https)
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41082 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.ws.ui/src/org/jboss/tools/ws/ui/dialogs/WSTesterURLInputsDialog.java
+++ plugins/org.jboss.tools.ws.ui/src/org/jboss/tools/ws/ui/dialogs/WSTesterURLInputsDialog.java
@@ -101,8 +101,11 @@ public class WSTesterURLInputsDialog extends TitleAreaDialog {
 			modified = modified.replace("//", "/"); //$NON-NLS-1$ //$NON-NLS-2$
 			
 			// fix JBIDE-11728
-			modified = modified.replace("http:/", "http://"); //$NON-NLS-1$ //$NON-NLS-2$
-			modified = modified.replace("https:/", "https://"); //$NON-NLS-1$ //$NON-NLS-2$
+			if (modified.startsWith("http:/") && !modified.startsWith("http://")) { //$NON-NLS-1$ //$NON-NLS-2$
+				modified = modified.replace("http:/", "http://"); //$NON-NLS-1$ //$NON-NLS-2$
+			} else if (modified.startsWith("https:/") && !modified.startsWith("https://")) { //$NON-NLS-1$ //$NON-NLS-2$
+				modified = modified.replace("https:/", "https://"); //$NON-NLS-1$ //$NON-NLS-2$
+			}
 			
 			return modified;
 		}
