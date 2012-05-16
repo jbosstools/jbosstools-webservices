988b1eb77c16ca7d79843811cba9db46b47a549f
Brian Fitzpatrick <bfitzpat@redhat.com>
2012-05-16 15:23:08 +0000

    JBIDE-11294 - working around URL issue in a separate spot
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@41072 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.ws.ui/src/org/jboss/tools/ws/ui/views/JAXRSWSTestView2.java
+++ plugins/org.jboss.tools.ws.ui/src/org/jboss/tools/ws/ui/views/JAXRSWSTestView2.java
@@ -1858,7 +1858,11 @@ public class JAXRSWSTestView2 extends ViewPart {
 						JBossWSUIPlugin.PLUGIN_ID, 
 						JBossWSUIMessages.JAXRSWSTestView_Exception_Status + e.getLocalizedMessage());
 				status.setResultsText(e.toString());
-				JBossWSUIPlugin.log(e);
+				// this fix is to address JBIDE-11294 and the fact that we shouldn't actually log this exception from deep 
+				// within the WS API. 
+				if ((!e.getLocalizedMessage().contains("Unsupported endpoint address: REPLACE_WITH_ACTUAL_URL"))) { //$NON-NLS-1$
+					JBossWSUIPlugin.log(e);
+				}
 				return status;
 			}
 			
