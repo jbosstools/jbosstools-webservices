645457d91f4a93da3594716194b70f4512a5e1fb
Rob Stryker <rob@oxbeef.net>
2015-11-04 03:04:38 -0500

    JBIDE-21050 - allow JaxrsEndpointModuleArtifact to have serialization support and extend ModuleArtifactDelegate for integration with launchbar and more efficient wtp-servertools functionality

--- plugins/org.jboss.tools.ws.jaxrs.ui/src/org/jboss/tools/ws/jaxrs/ui/internal/launcher/JaxrsEndpointModuleArtifact.java
+++ plugins/org.jboss.tools.ws.jaxrs.ui/src/org/jboss/tools/ws/jaxrs/ui/internal/launcher/JaxrsEndpointModuleArtifact.java
@@ -12,7 +12,9 @@
 package org.jboss.tools.ws.jaxrs.ui.internal.launcher;
 
 import org.eclipse.wst.server.core.IModule;
-import org.eclipse.wst.server.core.IModuleArtifact;
+import org.eclipse.wst.server.core.IServer;
+import org.eclipse.wst.server.core.ServerUtil;
+import org.eclipse.wst.server.core.model.ModuleArtifactDelegate;
 import org.jboss.tools.ws.jaxrs.core.metamodel.domain.IJaxrsEndpoint;
 
 /**
@@ -21,13 +23,21 @@ import org.jboss.tools.ws.jaxrs.core.metamodel.domain.IJaxrsEndpoint;
  * @author Xavier Coulon
  *
  */
-public class JaxrsEndpointModuleArtifact implements IModuleArtifact {
+public class JaxrsEndpointModuleArtifact extends ModuleArtifactDelegate {
 
 	/** the module.*/
-	private final IModule module;
+	private IModule module;
 	
 	/** the JAX-RS Endpoint that should be called.*/
-	private final IJaxrsEndpoint endpoint;
+	private IJaxrsEndpoint endpoint;
+	private String endpointURI;
+	private String httpVerb;
+	
+	
+	// No-arg constructor used for deserialization
+	public JaxrsEndpointModuleArtifact() {
+		
+	}
 	
 	/**
 	 * Full constructor.
@@ -44,8 +54,81 @@ public class JaxrsEndpointModuleArtifact implements IModuleArtifact {
 		return module;
 	}
 	
+	@Deprecated
 	public IJaxrsEndpoint getEndpoint() {
 		return endpoint;
 	}
 
+	public String getEndpointURI() {
+		if( endpointURI == null && endpoint != null) {
+			endpointURI = computeEndpointURI(module, endpoint);
+		}
+		return endpointURI;
+	}
+	
+	public String getHttpVerb() {
+		if( endpoint != null )
+			return endpoint.getHttpMethod().getHttpVerb();
+		return httpVerb;
+	}
+	
+	/**
+	 * Creates the endpoint URI from the endpoint, by looking at the servers on which the module is deployed.
+	 * 
+	 * @param element
+	 * @return
+	 */
+	private String computeEndpointURI(final IModule module, final IJaxrsEndpoint endpoint) {
+		String uriPathTemplate = endpoint.getUriPathTemplate();
+		if (!uriPathTemplate.startsWith("/")) { //$NON-NLS-1$
+			uriPathTemplate = "/"+uriPathTemplate; //$NON-NLS-1$
+		}
+		String fullUriPathTemplate = module.getName() + uriPathTemplate;
+		// check to see if this project has been deployed...
+		IServer[] servers = ServerUtil.getServersByModule(module, null);
+		if (servers == null || servers.length == 0) {
+			return "http://[domain]:[port]/" + fullUriPathTemplate; //$NON-NLS-1$
+		}
+		// if it's been deployed, we can grab the domain and web port
+		String domain = servers[0].getHost();
+		String webport = servers[0].getAttribute("org.jboss.ide.eclipse.as.core.server.webPort", "8080");//$NON-NLS-1$ //$NON-NLS-2$
+		return "http://" + domain + ':' + webport + "/" + fullUriPathTemplate; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+
+	}
+
+	@Override
+	public String getName() {
+		return getEndpointURI();
+	}
+	
+	// Very simple serialization, three lines:
+	// 1)  module id
+	// 2)  endpoint uri
+	// 3)  http verb
+	public String serialize() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(module.getId());
+		sb.append("\n");
+		sb.append(getEndpointURI());
+		sb.append("\n");
+		sb.append(getHttpVerb());
+		return sb.toString();
+	}
+
+	/**
+	 * Deserialize this object from a serialized string.
+	 * 
+	 * @param s a serialized string.
+	 */
+	public void deserialize(String s) {
+		String[] byLine = s.split("\n");
+		if( byLine != null ) {
+			if( byLine.length > 0 && byLine[0] != null)
+				this.module = ServerUtil.getModule(byLine[0]);
+			if( byLine.length > 1 && byLine[1] != null)
+				this.endpointURI = byLine[1];
+			if( byLine.length > 2 && byLine[2] != null)
+				this.httpVerb = byLine[2];
+		}
+	}
 }
